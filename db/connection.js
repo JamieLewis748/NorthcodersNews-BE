e983c953372148ad1a9232291925f7667aa0a25d
Jamie Lewis <Jamie@Jamies-MacBook-Air.local>
2023-08-18 11:46:13 +0100

    removed second PGDATAVASE not set

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,6 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
+
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
@@ -12,10 +13,6 @@ const config = {};
 if (ENV === 'production') {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
-}
-
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
 }
 
 module.exports = new Pool(config);
