cc055cb78124d920c851808e5dbdbf2d014dadf6
Jamie Lewis <j.h.lewis89@gmail.com>
2023-11-14 09:42:43 +0700

    adds body to getAllArticles

--- models/articles.model.js
+++ models/articles.model.js
@@ -36,6 +36,7 @@ exports.selectAllArticles = (topic, sort_by = 'created_at', order = 'desc') => {
         articles.created_at,
         articles.votes,
         articles.article_img_url,
+        articles.body,
         CAST(COUNT(comments.comment_id) AS INT) AS comment_count
         FROM articles
         LEFT JOIN comments ON articles.article_id = comments.article_id`;
